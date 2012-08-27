598aaad7c8f9ba196d4b68634a228f98c76d7ded
jspears <speajus@gmail.com>
2012-08-27 13:32:48 -0400

    *small fix to allow plugins to function correctly.

--- plugins/generator/generator.js
+++ plugins/generator/generator.js
@@ -3,6 +3,8 @@ var Plugin = require('../../lib/plugin-api'), util = require('util'), _u = requi
 var GeneratorPlugin = function (options) {
     Plugin.apply(this, arguments);
     this.pluginUrl = this.baseUrl;
+    this.styles = ['css/bootstrap.min.css', 'css/styles', 'js/libs/table/jquery.sorter.css', 'js/libs/backbone-forms/src/backbone-forms.css', 'js/libs/backbone-forms/test/lib/jquery-ui/flick/jquery-ui-1.8.14.custom.css', 'js/libs/jquery-miniColors/jquery.miniColors.css'];
+
 }
 
 util.inherits(GeneratorPlugin, Plugin);
@@ -25,7 +27,7 @@ GeneratorPlugin.prototype.filters = function (options) {
             'options':options
         };
         if (_u.isFunction(res.local)) {
-            _u.each(locals, function (v,k) {
+            _u.each(locals, function (v, k) {
                 res.local(k, v);
             })
         } else {
@@ -33,6 +35,14 @@ GeneratorPlugin.prototype.filters = function (options) {
         }
         next();
     }.bind(this));
+    /*
+     <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/styles.css" rel="stylesheet">
+     <link href="js/libs/table/jquery.sorter.css" rel="stylesheet">
+     <link href="js/libs/backbone-forms/src/backbone-forms.css" rel="stylesheet">
+     <link href="js/libs/backbone-forms/test/lib/jquery-ui/flick/jquery-ui-1.8.14.custom.css" rel="stylesheet">
+     <link href="js/libs/jquery-miniColors/jquery.miniColors.css" rel="stylesheet">
+     */
 
 }
 var extRe = /\.(js|html|css|htm)$/i;
@@ -54,11 +64,15 @@ GeneratorPlugin.prototype.routes = function (options) {
 
     var app = this.app;
     var base = this.baseUrl;
-    if (this.options.index) {
-        app.get(base + this.options.index, function (req, res, next) {
-            this.generate(res, 'index.html', makeOptions(req), next);
-        }.bind(this))
-    }
+    var index = this.options.index || 'index.html';
+
+    app.get(base + index, function (req, res, next) {
+
+
+        this.local(res, 'styles', this.styles);
+        console.log('styles generated');
+        this.generate(res, 'index.html', makeOptions(req), next);
+    }.bind(this))
 
     app.get(base, function (req, res, next) {
         res.redirect(this.baseUrl + (this.options.index || 'index.html'));
